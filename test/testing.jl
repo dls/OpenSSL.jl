8369dc4bf8afc6a74088d9008d152f0deb189b77
David Salamon <dls@lithp.org>
2013-09-03 02:10:13 -0700

    adds cryptographic padding functions

--- test/testing.jl
+++ test/testing.jl
@@ -3,7 +3,7 @@ macro assert_equal(a, b)
         aval = $a
         bval = $b
         if aval != bval
-            println("Failure: (", aval, " == ", bval, ")")
+            println("Failure: (", aval, " != ", bval, "), expected it to ==")
         else
             print(".")
         end
@@ -15,7 +15,7 @@ macro assert_not_equal(a, b)
         aval = $a
         bval = $b
         if aval == bval
-            println("Failure: (", aval, " != ", bval, ")")
+            println("Failure: (", aval, " == ", bval, "), expected it to !=")
         else
             print(".")
         end
