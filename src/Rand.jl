2328f98b1a3a0e66314948b2c1b27fdec56327cf
David Salamon <dls@lithp.org>
2013-09-05 03:32:21 -0700

    fixes strange type hierarchy issue with Ints and Int64 -- TODO come back to this

--- src/Rand.jl
+++ src/Rand.jl
@@ -5,12 +5,12 @@ export cryptographic_rand, create_block_cipher_key
 include("gen/libcrypto_common.jl")
 include("gen/rand.h.jl")
 
-cryptographic_rand(len :: Int) = cryptographic_rand(zeros(Uint8, len))
+cryptographic_rand(len) = cryptographic_rand(zeros(Uint8, len))
 function cryptographic_rand(a :: Array{Uint8})
     RAND_bytes(a, length(a))
     a
 end
 
-create_block_cipher_key(size :: Int) = cryptographic_rand(size)
+create_block_cipher_key(size) = cryptographic_rand(size)
 
 end # module Rand
